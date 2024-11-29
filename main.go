5d57abff2c64351a361947e519f9b98a69ef666f
helsasp <helsasp@gmail.com>
2024-11-29 13:30:04 +0700

    feat: add model category

--- main.go
+++ main.go
@@ -1,19 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
-	"go-final-project/config" 
+	"go-final-project/config"
+	"go-final-project/models"
+
 )
 
 func main() {
-	
-	db := config.GetDB() 
+	db := config.GetDB()
+
+	err := db.AutoMigrate(&models.Category{})
+	if err != nil {
+		panic("Failed to migrate database: " + err.Error())
+	}
+	fmt.Println("Database migrated successfully!")
+
 	r := gin.Default()
 
-	
+
 	r.LoadHTMLGlob("templates/*")
 
-	
+
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "home.html", nil)
 	})
---
+++ models/category.go
@@ -0,0 +1,9 @@
+package models
+
+import "gorm.io/gorm"
+
+type Category struct {
+    gorm.Model           
+    Name        string    `gorm:"not null" form:"name"`       
+    Description string    `gorm:"not null" form:"description"` 
+}

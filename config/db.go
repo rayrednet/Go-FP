d473b77470345ed01c805148ba7407ebc4072e97
Rayssa Ravelia <rayssa.sibarani@gmail.com>
2024-12-10 15:24:10 +0700

    refactor: replace hardcoded database path with environment variables

--- config/db.go
+++ config/db.go
@@ -5,22 +5,37 @@ import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 )
 
 var DB *gorm.DB
 
 func GetDB() *gorm.DB {
 	if DB == nil {
-		
-		dsn := "root:helsasp@tcp(127.0.0.1:3306)/fp_pbkk?charset=utf8mb4&parseTime=True&loc=Local"
-		
-		
-		var err error
+		// Load environment variables
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file: %v", err)
+		}
+
+		// Get environment variables
+		dbUser := os.Getenv("DB_USER")
+		dbPassword := os.Getenv("DB_PASSWORD")
+		dbHost := os.Getenv("DB_HOST")
+		dbPort := os.Getenv("DB_PORT")
+		dbName := os.Getenv("DB_NAME")
+
+		// Create the DSN
+		dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+			dbUser, dbPassword, dbHost, dbPort, dbName)
+
+		// Connect to the database
 		DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 		if err != nil {
 			log.Fatalf("Error connecting to the database: %v", err)
 		}
-
 
 		fmt.Println("Connected to the database successfully")
 	}

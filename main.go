472bc4ac1a79a8493984e762f020e3268a2c2b62
Rayssa Ravelia <rayssa.sibarani@gmail.com>
2024-12-10 17:57:56 +0700

    Refactor Navbar and Footer Code, Add Favicon, Improve Responsiveness, and Modularize CSS
    - Split Navbar and Footer into individual reusable templates for better code organization.
    - Added a favicon to enhance branding and improve browser tab visibility.
    - Updated Navbar to always stay on top using sticky positioning for better UX.
    - Centralized all styles into a separate `style.css` file for modularity and maintainability.
    - Enhanced Navbar styling for better alignment and responsiveness.
    - Improved alignment of Navbar text with the hero section for a polished look.

--- main.go
+++ main.go
@@ -7,14 +7,12 @@ import (
 	"go-final-project/controllers"
 	"go-final-project/models"
 	"go-final-project/seeders"
-	
 )
 
 func main() {
 
 	db := config.GetDB()
 
-	
 	err := db.AutoMigrate(&models.Category{}, &models.Product{}, &models.Review{})
 	if err != nil {
 		panic("Failed to migrate database: " + err.Error())
@@ -25,8 +23,15 @@ func main() {
 
 	r := gin.Default()
 
-	r.LoadHTMLGlob("templates/*")
+	// Serve static files from the "static" directory
+	r.Static("/static", "./static")
 
+	// Add route for favicon
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.File("./static/images/favicon.ico")
+	})
+
+	r.LoadHTMLGlob("templates/*")
 
 	r.GET("/categories", controllers.GetCategories)
 	r.GET("/categories/create", controllers.NewCategory)
@@ -35,19 +40,17 @@ func main() {
 	r.POST("/categories/:id/update", controllers.UpdateCategory)
 	r.POST("/categories/:id/delete", controllers.DeleteCategory)
 
-	
-	r.GET("/products", controllers.GetProducts)             
-	r.GET("/products/create", controllers.NewProduct)        
-	r.POST("/products", controllers.CreateProduct)          
-	r.GET("/products/:id/edit", controllers.EditProduct)     
-	r.POST("/products/:id/update", controllers.UpdateProduct) 
-	r.POST("/products/:id/delete", controllers.DeleteProduct) 
-	r.GET("/products/:id", controllers.ShowProduct)         
+	r.GET("/products", controllers.GetProducts)
+	r.GET("/products/create", controllers.NewProduct)
+	r.POST("/products", controllers.CreateProduct)
+	r.GET("/products/:id/edit", controllers.EditProduct)
+	r.POST("/products/:id/update", controllers.UpdateProduct)
+	r.POST("/products/:id/delete", controllers.DeleteProduct)
+	r.GET("/products/:id", controllers.ShowProduct)
 
-	r.GET("/reviews", controllers.GetReviews)        
-	r.GET("/reviews/create", controllers.NewReview)  
-	r.POST("/reviews", controllers.CreateReview)    
-
+	r.GET("/reviews", controllers.GetReviews)
+	r.GET("/reviews/create", controllers.NewReview)
+	r.POST("/reviews", controllers.CreateReview)
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(200, "home.html", nil)
